7c6234ec8eef0b53e1cfc62202f553d4fcd9d7e8
TahirKhadim <62652885+TahirKhadim@users.noreply.github.com>
2023-10-21 01:37:06 -0700

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,45 @@
+
+
+gsap.from("#right", {
+    scrollTrigger: {
+        trigger: "#right",
+        scroller: "body"
+
+    },
+    x: "200",
+    duration: 1,
+    delay: 1,
+
+
+})
+gsap.from("#left", {
+    scrollTrigger: {
+        trigger: "#left",
+        scroller: "body"
+
+    },
+    x: "-200",
+    duration: 1,
+    delay: 1,
+
+
+})
+
+gsap.from("#down", {
+    scrollTrigger: {
+        trigger: "#down",
+        scroller: "body"
+
+    },
+    y: "20px",
+    duration: 1,
+    delay: 1,
+
+
+})
+
+// tl.from(".img-fluid", {
+//     x: "-600",
+//     duration: 1,
+//     delay: 0.5,
+// }) 
